c2c92f6c0f72f6ca1d8b9102fb108e1d7cc00c82
Liam <liambhetherington@gmail.com>
2019-10-15 17:35:23 -0700

    finds first element in a given array

--- head.js
+++ head.js
@@ -1,3 +1,7 @@
+const head = function (arr) {
+ return arr[0];
+};
+
 const assertEqual = function(actual, expected) {
  if(actual === expected) {
   return `Assertion Passed: ${actual} === ${expected}`;
@@ -6,5 +10,6 @@ const assertEqual = function(actual, expected) {
  }
 };
 
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, 1));
+console.log(assertEqual(head([]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+
